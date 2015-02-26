a68de7c7dc1e465d220348166980f9df2feb9275
akshaydesai <akkiview@Akshays-MacBook-Air-2.local>
2015-02-26 15:49:45 -0500

    Dashboard
    Designed  Temporary Dashboard

---
+++ dashboard/includes/subNavigation.php
@@ -0,0 +1,11 @@
+	<div class="small-12 medium-3 large-3 columns">
+	<br/>
+		<div class="panel callout radius">
+			<ul class="side-nav">
+			  <li><a href="#">Link 1</a></li>
+			  <li><a href="#">Link 2</a></li>
+			  <li><a href="#">Link 3</a></li>
+			  <li><a href="#">Link 4</a></li>
+			</ul>
+		</div>	
+	</div>
--- dashboard/index.php
+++ dashboard/index.php
@@ -6,8 +6,135 @@
 
 <?php include("includes/navigation.php") ?>
 
+<div class="row">
+	<?php include("includes/subNavigation.php") ?>
+	<div class="small-12 medium-9 large-9 columns">
+		<div class="row">
 
-<?php include("includes/subNavigation.php") ?>
+			<br/>
+			<div class="small-9 medium-9 large-9  columns">
+				<h2>Company Name</h2>
+				<h4 class="subheader"><i><small>-1350 Steels Avenue, M9V 1N8</small></i></h4>
+			</div>
+			
+			<div class="small-3 medium-3 large-3 columns"> <!-- Profile picture of company or user-->
+				<div class="panel callout radius text-center">
+					<p></p>
+					<p>Logo</p>
+					<p></p>
+				</div>
+			</div>
+
+		</div>
+
+		<div class="small-12 medium-12 large-12  columns">
+			<div class="row">
+				<div class="small-12 medium-12 large-12 columns">
+					<div class="panel text-center">
+						<h4>Active shifts</h4>
+					</div>					
+				</div>
+			</div>
+			<div class="row">
+				<div class="small-3 medium-3 large-3 columns">
+					<dl>
+						<dt>Jobs</dt>
+						<dd>Job 1</dd>
+						<dd>Job 2</dd>
+						<dd>Job 3</dd>
+						<dd>Job 4</dd>
+					</dl>
+				</div>
+				<div class="small-3 medium-3 large-3 columns">
+					<dl>
+						<dt>description</dt>
+						<dd>description 1</dd>
+						<dd>description 2</dd>
+						<dd>description 3</dd>
+						<dd>description 4</dd>
+					</dl>
+				</div>
+				<div class="small-3 medium-3 large-3 columns">
+					<dl>
+						<dt>Required</dt>
+						<dd>100</dd>
+						<dd>50</dd>
+						<dd>100</dd>
+						<dd>70</dd>
+					</dl>
+				</div>
+				<div class="small-3 medium-3 large-3 columns">
+					<dl>
+						<dt>Applied</dt>
+						<dd>57</dd>
+						<dd>50</dd>
+						<dd>34</dd>
+						<dd>55</dd>
+					</dl>				
+				</div>
+
+			</div>			
+			<hr/>
+		</div>
+
+		<div class="small-12 medium-12 large-12  columns">
+			<div class="row">
+				<div class="small-12 medium-12 large-12 columns">
+					<div class="panel text-center">
+						<h4>Supervisors</h4>
+					</div>					
+				</div>
+			</div>
+			<div class="row">
+				<div class="small-3 medium-3 large-3 columns">
+					<dl>
+						<dt>Name</dt>
+						<dd>Job 1</dd>
+						<dd>Job 2</dd>
+						<dd>Job 3</dd>
+						<dd>Job 4</dd>
+					</dl>
+				</div>
+				<div class="small-3 medium-3 large-3 columns">
+					<dl>
+						<dt>Designation</dt>
+						<dd>description 1</dd>
+						<dd>description 2</dd>
+						<dd>description 3</dd>
+						<dd>description 4</dd>
+					</dl>
+				</div>
+				<div class="small-3 medium-3 large-3 columns">
+					<dl>
+						<dt>Email-Id</dt>
+						<dd>100</dd>
+						<dd>50</dd>
+						<dd>100</dd>
+						<dd>70</dd>
+					</dl>
+				</div>
+				<div class="small-3 medium-3 large-3 columns">
+					<dl>
+						<dt>Action</dt>
+						<dd>Delete/Edit</dd>
+						<dd>Delete/Edit</dd>
+						<dd>Delete/Edit</dd>
+						<dd>Delete/Edit</dd>
+					</dl>				
+				</div>
+
+			</div>			
+			<div class="row">
+				<div class="small-12 medium-12 large-12 columns">
+					<a href="#" class="button expand">+ Add New Supervisor</a>					
+				</div>
+			</div>
+			<hr/>
+		</div>
+
+	</div>
+</div>
+
 
 
 
